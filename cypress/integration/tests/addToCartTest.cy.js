371763bccfd20e0559fa86731fa2f599b432065f
Vaibh-Sontakke <vssontakke19@gmail.com>
2024-04-11 00:01:27 +0530

    Commiting yml file

---
+++ cypress/integration/tests/addToCartTest.cy.js
@@ -0,0 +1,20 @@
+import { homePage } from "../../pages/homePage"
+const homePageObj = new homePage()
+import testData from '../../fixtures/testData.json'
+
+
+describe(' test automation', () => {
+    before(() => {
+        cy.login(testData.login.username, testData.login.password)
+    })
+
+
+    it('Add To Cart flow', () => {
+        Cypress.config('defaultCommandTimeout', 10000);
+        homePageObj.searchProduct(testData.product.productName)
+        homePageObj.addToCart()
+        homePageObj.verifySucessMessage().should('contain', testData.message.successMessage).and('contain', testData.product.productName);
+
+
+    })
+})
